86c4d61665d1f51e492ea502b65cc872dc9d5d0f
itsbamee <jan9239@naver.com>
2023-11-18 09:49:12 +0900

    스크롤시 버튼활성화 및 기준점 변경

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+  window.scrollY : 브라우저의 현재 스크롤 거리 값 (동적=계속바뀌는 값)
+  DOM.offsetTop : 문서 상단 끝에서부터 현재 DOM 요소까지의 세로 위치값 (정적)
+  window.scrollTo({top: 이동할 세로 위치값})
+*/
+
+const [main, ul] = document.body.children;
+const secs = main.querySelectorAll('section');
+const btns = ul.querySelectorAll('li');
+//스크롤이 활성화되는 기준점 변경 위치값
+const baseLine = -window.innerHeight / 2;
+
+window.addEventListener('scroll', () => {
+	const scroll = window.scrollY;
+
+	secs.forEach((_, idx) => {
+		if (scroll >= secs[idx].offsetTop + baseLine) {
+			btns.forEach((btn) => btn.classList.remove('on'));
+			btns[idx].classList.add('on');
+		}
+	});
+});
+
+btns[idx].addEventListener('click', () => {
+	window.scrollTo({ top: main.children[idx].offsetTop, behavior: 'smooth' });
+});
