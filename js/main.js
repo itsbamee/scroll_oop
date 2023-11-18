c15e140581e3f30cfc258614b16fc32f63c57c63
itsbamee <jan9239@naver.com>
2023-11-18 10:27:54 +0900

    활성화버튼 재이벤트 방지, 모션중 재이벤트 방지

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-/*
-  window.scrollY : 브라우저의 현재 스크롤 거리 값 (동적=계속바뀌는 값)
-  DOM.offsetTop : 문서 상단 끝에서부터 현재 DOM 요소까지의 세로 위치값 (정적)
-  window.scrollTo({top: 이동할 세로 위치값})
-*/
-
 const [main, ul] = document.body.children;
 const secs = main.querySelectorAll('section');
 const btns = ul.querySelectorAll('li');
 const baseLine = -window.innerHeight / 2;
 const speed = 500;
+let preventEvent = false;
 
 window.addEventListener('scroll', activation);
 
 btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => moveScroll(idx, speed));
+	btn.addEventListener('click', (e) => {
+		if (e.currentTarget.classList.contains('on') || preventEvent) return;
+		preventEvent = true;
+		moveScroll(idx, speed);
+	});
 });
 
 function activation() {
 	const scroll = window.scrollY;
-
 	secs.forEach((_, idx) => {
 		if (scroll >= secs[idx].offsetTop + baseLine) {
 			btns.forEach((btn) => btn.classList.remove('on'));
@@ -28,5 +26,10 @@ function activation() {
 }
 
 function moveScroll(idx, speed) {
-	new Anime(window, { scroll: secs[idx].offsetTop }, { duration: speed, easeType: 'ease1' });
+	console.log('move');
+	new Anime(
+		window,
+		{ scroll: secs[idx].offsetTop },
+		{ duration: speed, callback: () => (preventEvent = false) }
+	);
 }
