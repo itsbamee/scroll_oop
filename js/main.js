6aa99aa163bc57bf3ecfc7de2519436e5aec224b
itsbamee <jan9239@naver.com>
2023-11-18 11:12:43 +0900

    동적 스크롤 버튼 생성

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-const [main, ul] = document.body.children;
+const main = document.querySelector('main');
 const secs = main.querySelectorAll('section');
-const btns = ul.querySelectorAll('li');
+const btns = createBtns(secs.length);
 const baseLine = -window.innerHeight / 2;
 const speed = 500;
 let preventEvent = false;
 
+activation();
 window.addEventListener('scroll', activation);
+window.addEventListener('resize', modifyPos);
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
@@ -15,6 +17,16 @@ btns.forEach((btn, idx) => {
 	});
 });
 
+function createBtns(num) {
+	const ul = document.createElement('ul');
+	Array(num)
+		.fill()
+		.forEach(() => ul.append(document.createElement('li')));
+
+	document.body.append(ul);
+	return document.querySelectorAll('ul li');
+}
+
 function activation() {
 	const scroll = window.scrollY;
 	secs.forEach((_, idx) => {
@@ -23,6 +35,13 @@ function activation() {
 			btns[idx].classList.add('on');
 		}
 	});
+}
+
+function modifyPos() {
+	const active = ul.querySelector('li.on');
+	const active_index = Array.from(btns).indexOf(active);
+	console.log(active_index);
+	window.scrollTo({ top: secs[active_index].offsetTop });
 }
 
 function moveScroll(idx, speed) {
