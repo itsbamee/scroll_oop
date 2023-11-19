7276704e5530bf3bc85ef5b116fe4bdf3703f185
itsbamee <jan9239@naver.com>
2023-11-19 09:14:57 +0900

    scroll 이벤트에 연결되어있는 activation에 throttling 적용

--- js/main.js
+++ js/main.js
@@ -5,12 +5,23 @@ const baseLine = -window.innerHeight / 2;
 const speed = 500;
 let preventEvent = false;
 let isAutoScroll = true;
-let isBtns = false;
+let eventBlocker = null;
 
 //단기간에 많이 발생하는 이벤트 (resize, scroll) 1초에 60번 반복 (화면주사율)
 //throttle : 이벤트 발생을 강제로 throttling해서 불필요한 반복되는 이벤트 핸들러 함수 호출을 방지
 activation();
-window.addEventListener('scroll', activation);
+
+//스크롤 이벤트가 발생할 때마다 eventBlocker라는 전역변수에 setTimeout이 리턴하는 1씩 증가하는 값을 담음
+window.addEventListener('scroll', () => {
+	if (eventBlocker) return;
+	//setTimeout이 실행될때마다 eventBlocker에는 true가 되므로(값이 담겨있다는 뜻)
+	//setTimeout호출동안 0.2s 동안은 activation의 호출을 강제로 막아줌
+	eventBlocker = setTimeout(() => {
+		activation();
+		//setTimeout이 종료되면 eventBlocker값이 null(false)가 되므로 다시 이벤트 호출이 가능해짐
+		eventBlocker = null;
+	}, 200);
+});
 window.addEventListener('resize', modifyPos);
 isAutoScroll && window.addEventListener('mousewheel', autoScroll, { passive: false });
 
